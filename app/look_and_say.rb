69b5d6253d1973b5c55b11dff6c03050ce3cf317
Ilia Osipov <ya.ilia.osipov@yandex.ru>
2020-11-25 18:41:33 +0300

    Improve client interface

--- app/look_and_say.rb
+++ app/look_and_say.rb
@@ -2,20 +2,48 @@
 
 require_relative 'sequence'
 require_relative 'look_say_member'
+require 'optparse'
+require 'benchmark'
 
-puts 'basic:'
-sequence = Sequence.new(LookSayMember)
-sequence.to_enum.take(10).each { |m| puts m.value }
-sequence.each(10) do |m|
-  puts m.value
-end
+options = {}
+OptionParser.new do |parser|
+  parser.banner = 'Look-and-say sequence builder'
+  parser.on('-c', '--cache', 'Cache sequence') do
+    options[:cache] = true
+  end
+  parser.on('-l=LENGTH', '--length=LENGTH', Integer, 'Members to generate') do |l|
+    options[:length] = l
+  end
 
-puts 'cached:'
-sequence2 = SequenceCached.new(LookSayMember)
-sequence2.each(10) do |m|
-  puts m.value
-end
-sequence2.to_enum.take(10).each do |m|
-  puts m.value
-end
+  parser.on('-r=COUNT', '--rerun=COUNT', Integer, 'How many times to run') do |c|
+    options[:rerun] = c
+  end
 
+  parser.on('-b', '--benchmark', 'Print only performance results') do
+    options[:benchmark] = true
+  end
+
+  parser.on('-h', '--help', 'Prints this help') do
+    puts parser
+    exit
+  end
+end.parse!
+
+options[:rerun] ||= 1
+options[:length] ||= 15
+
+puts 'Using cache' if options[:cache]
+puts "Generating #{options[:length] ||= 20} members"
+puts "#{options[:rerun]} times"
+puts 'Silently' if options[:benchmark]
+
+
+sequence = options[:cache] ? SequenceCached.new(LookSayMember) : Sequence.new(LookSayMember)
+(0...options[:rerun]).each do
+  r = Benchmark.measure do
+    sequence.to_enum.take(options[:length]).each do |m, i|
+      puts "#{i}: #{m.value}" unless options[:benchmark]
+    end
+  end
+  puts r
+end
